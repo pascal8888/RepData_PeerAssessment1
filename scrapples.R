2ab979b55442b566b7386f847b1854baeb86ff04
James Schacht <pascal888@yahoo.com>
2015-07-19 01:14:07 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- scrapples.R
+++ scrapples.R
@@ -1,2 +1,12 @@
 df <- aggregate.ts(repdata,as.list(repdata$date),nfrequency=1,FUN=sum, simplify=T)
 repdata <- as.data.frame(aggregate.ts(repdata,as.list(repdata$date),nfrequency=1,FUN=sum, simplify=T))
+repdata$date <- as.Date(repdata$date)
+
+byDate <- aggregate(steps ~ date, repdata, sum, na.rm = TRUE)
+avgSteps <- aggregate(steps ~ interval, repdata, mean, na.rm = TRUE)
+maxStepsCount <- which.max(avgSteps$steps)
+hist(totalStepsDate$steps, col = "blue",
+     main = "Total Number Steps Per Day", xlab = "Steps Per Day")
+repdata <- mutate(repdata, Day = paste(year(date),yday(date)))
+repdata$Day <- as.numeric(sub(" ","",repdata$Day))
+plot(as.Date(repdata$date),repdata$steps, repdata$interval, xlab= "Date", ylab= "Steps", type="l", col='blue')
